82c1d0b2c72b511c6ebbfa232fd812fcf6e9d626
Jakub Woźniak <jakubwozniak0611@gmail.com>
2022-06-15 01:38:12 +0200

    Pushed everything to date

---
+++ zadania_dodatkowe/zadania_dodatkowe/src/zad1.js
@@ -0,0 +1,20 @@
+"use strict";
+const getRandomNumber = () => {
+    return Math.floor(Math.random() * 1000);
+};
+const getNumber = (cb) => {
+    setTimeout(() => {
+        cb(getRandomNumber());
+    }, getRandomNumber() * 5);
+};
+
+const asyncSort = (dim) => {
+    const unsortedArray = [...Array(dim).keys()].map(async (_) => {
+        return getNumber((n) => n + 1);
+    });
+    
+    return unsortedArray 
+};
+
+console.log(asyncSort(5))
+
